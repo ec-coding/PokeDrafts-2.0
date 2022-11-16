b5eba1f314d5c83ab8e8d90afc9cb8afc8cf3537
Edwin Choi <edwin.choi.wdev@gmail.com>
2022-11-15 18:09:13 -0600

    auth wip

--- node/server.js
+++ node/server.js
@@ -12,6 +12,8 @@ const {MongoClient, ObjectId} = require('mongodb')
 const connectDB = require('./config/db')
 const bodyParser = require('body-parser')
 const app = express()
+const { OAuth2Client } = require("google-auth-library");
+const jwt = require("jsonwebtoken");
 var engines = require('consolidate')
 const cors = require ('cors');
 const mainRoutes = require('./routes/main')
@@ -98,3 +100,94 @@ app.listen(
     PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 )
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+async function verifyGoogleToken(token) {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    return { payload: ticket.getPayload() };
+  } catch (error) {
+    return { error: "Invalid user detected. Please try again" };
+  }
+}
+
+app.post("/signup", async (req, res) => {
+    try {
+      // console.log({ verified: verifyGoogleToken(req.body.credential) });
+      if (req.body.credential) {
+        const verificationResponse = await verifyGoogleToken(req.body.credential);
+  
+        if (verificationResponse.error) {
+          return res.status(400).json({
+            message: verificationResponse.error,
+          });
+        }
+  
+        const profile = verificationResponse?.payload;
+  
+        DB.push(profile);
+  
+        res.status(201).json({
+          message: "Signup was successful",
+          user: {
+            firstName: profile?.given_name,
+            lastName: profile?.family_name,
+            picture: profile?.picture,
+            email: profile?.email,
+            token: jwt.sign({ email: profile?.email }, "myScret", {
+              expiresIn: "1d",
+            }),
+          },
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "An error occurred. Registration failed.",
+      });
+    }
+  });
+
+  app.post("/login", async (req, res) => {
+    try {
+      if (req.body.credential) {
+        const verificationResponse = await verifyGoogleToken(req.body.credential);
+        if (verificationResponse.error) {
+          return res.status(400).json({
+            message: verificationResponse.error,
+          });
+        }
+  
+        const profile = verificationResponse?.payload;
+  
+        const existsInDB = DB.find((person) => person?.email === profile?.email);
+  
+        if (!existsInDB) {
+          return res.status(400).json({
+            message: "You are not registered. Please sign up",
+          });
+        }
+  
+        res.status(201).json({
+          message: "Login was successful",
+          user: {
+            firstName: profile?.given_name,
+            lastName: profile?.family_name,
+            picture: profile?.picture,
+            email: profile?.email,
+            token: jwt.sign({ email: profile?.email }, process.env.JWT_SECRET, {
+              expiresIn: "1d",
+            }),
+          },
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error?.message || error,
+      });
+    }
+  });
