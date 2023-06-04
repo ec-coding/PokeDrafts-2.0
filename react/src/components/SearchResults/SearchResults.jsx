40ee6e04bd05744315eba9dc4cd4aaac299dcf42
Edwin Choi <edwin.choi.wdev@gmail.com>
2023-06-04 14:45:57 -0500

    corrected css dependencies

--- react/src/components/SearchResults/SearchResults.jsx
+++ react/src/components/SearchResults/SearchResults.jsx
@@ -4,7 +4,7 @@ import ReactPaginate from 'react-paginate';
 import CardContext from '../../contexts/CardContext';
 // import PageCountContext from '../../contexts/PageCountContext';
 import Card from '../Card/Card'
-import '../../style/dropdown.css';
+import './dropdown.css';
 import './searchResults.scss';
 
 export default function SearchResults({ pageCount, pageSwitch, currentPage, totalCount, isLoading }) {
